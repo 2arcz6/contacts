fd5df7bd146fc32ac57947c52a13d01f26287b53
Ralph <ralph@valhallaonlineservices.com>
2021-05-23 06:39:45 +0800

    updated server url

--- contacts-react/src/http-common.js
+++ contacts-react/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8081/api",
+//   baseURL: "http://localhost:8081/api",
+  baseURL: "http://18.141.164.56:8081/api",
   headers: {
     "Content-type": "application/json"
   }
